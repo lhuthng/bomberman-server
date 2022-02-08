7748a01a49ee3b6f3818025e2c50a53fd962a1eb
Thang <lhuthng@gmail.com>
2022-02-08 10:50:36 +0100

    add socket.io

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
-
 const app = express();
+const server  = require('http').createServer(app);
+const port = process.env.port || 8080;
+
+server.listen(port, () => {
+  console.log(port);
+})
+
+const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/build'));
 
-app.listen(port, () => {
-  console.log(port);
-});
 app.get('/', (req, res, nxt) => {
   res.sendFile(__dirname + '/build/index.html');
+});
+
+io.on('connection', socket => {
+  console.log(`${socket} connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`${socket} disconnected`);
+  });
 });
